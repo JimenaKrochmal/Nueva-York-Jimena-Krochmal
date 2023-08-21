7d4b9cdd4207f27df4200ab233f19c630f77c893
Jimena Krochmal <jimenakrochmal@gmail.com>
2023-08-21 16:45:50 -0300

    posicion de los textos y creacion de boton de la pre entrega tres javascript

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 
+//creo y cargo las experiencias en mi HTML
 const experiencias = [
-    { id: 1, nombre: "hola", precio: 100 },
-    { id: 2, nombre: "chau", precio: 3 },
-    { id: 3, nombre: "cnd", precio: 56 },
-    { id: 4, nombre: "po", precio: 8908 },
+    { id: 1, nombre: "Puente de Brooklyn", precio: 100},
+    { id: 2, nombre: "Bryant Park", precio: 50 },
+    { id: 3, nombre: "Summit", precio: 56 },
+    { id: 4, nombre: "Rey León", precio: 150 },
 ];
+
+experiencias.forEach(experiencia => {
+    let div = document.createElement("div");
+    div.innerHTML = `
+    <h2> Id: ${experiencia.id} </h2>
+    <h3 class='experiencia-producto'> Nombre: ${experiencia.nombre} </h3>
+    <h3 class='experiencia-producto'> precio: $${experiencia.precio} </h3>
+    <button class= 'boton-carrito-agregar' onclick='agregarProducto(${experiencia.id})'> Agregar al carrito </button>
+    `
+
+    let main = document.querySelector('main')
+
+    main.append(div);
+});
+
+
 
 let carrito = [];
 
-// Cargar carrito desde el Local Storage al inicio
+// Cargo el carrito en caso de que ya tenga algo ingresado 
 const carritoGuardado = localStorage.getItem("carrito");
 if (carritoGuardado) {
     carrito = JSON.parse(carritoGuardado);
@@ -24,19 +41,8 @@ const agregarProducto = producto => {
     }
 }
 
-experiencias.forEach(experiencia => {
-    let div = document.createElement("div");
-    div.innerHTML = `
-    <h2> Id: ${experiencia.id} </h2>
-    <p class='experiencia-producto'> Nombre: ${experiencia.nombre} </p>
-    <p class='experiencia-producto'> precio: $${experiencia.precio} </p>
-    <button onclick='agregarProducto(${experiencia.id})'> Agregar al carrito </button>
-    `
 
-    let main = document.querySelector('main')
 
-    main.append(div);
-});
 
 
 
