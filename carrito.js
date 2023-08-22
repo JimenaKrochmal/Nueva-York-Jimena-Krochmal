1bde24317d82fee89e4689980512873c276a76e0
Jimena Krochmal <jimenakrochmal@gmail.com>
2023-08-22 18:04:39 -0300

    tercer pre entrega javascript

--- carrito.js
+++ carrito.js
@@ -5,16 +5,27 @@ if (carritoGuardado) {
     carrito = JSON.parse(carritoGuardado);
 }
 
+const borrarProducto = producto => {
+    const productoEnCarrito = carrito.filter(prod => prod.id !== producto);
+    console.log(productoEnCarrito);
+    if (productoEnCarrito) {
+        localStorage.setItem("carrito", JSON.stringify(productoEnCarrito));
+        location.reload()
+    }
+}
+
 carrito.forEach(carrito => {
     let div = document.createElement("div");
+    div.className = 'contenedor-div'
     div.innerHTML = `
     <h2> Id: ${carrito.id} </h2>
+    <img src= "${carrito.imagen}" alt=${carrito.nombre}/>
     <p class='experiencia-producto'> Nombre: ${carrito.nombre} </p>
     <p class='experiencia-producto'> precio: $${carrito.precio} </p>
-    <button onclick='agregarProducto(${carrito.id})'> Agregar al carrito </button>
+    <button onclick='borrarProducto(${carrito.id})'> Borrar del carrito </button>
     `
-
     let section = document.querySelector('section')
 
     section.append(div);
 });
+
--- main.js
+++ main.js
@@ -1,23 +1,24 @@
 
 //creo y cargo las experiencias en mi HTML
 const experiencias = [
-    { id: 1, nombre: "Puente de Brooklyn", precio: 100},
-    { id: 2, nombre: "Bryant Park", precio: 50 },
-    { id: 3, nombre: "Summit", precio: 56 },
-    { id: 4, nombre: "Rey León", precio: 150 },
+    { id: 1, nombre: "Monumentos", precio: 100, imagen: "https://cdn.milenio.com/uploads/media/2020/05/21/la-estructura-ha-sido-sede.jpg" },
+    { id: 2, nombre: "Parques", precio: 50, imagen: "https://drive.google.com/uc?export=view&id=1xIxVa7sKmx6_8-cLg1XkYawcnZmS2nFg" },
+    { id: 3, nombre: "Rascacielos", precio: 56, imagen: "https://media.architecturaldigest.com/photos/61708d2c231989f8d88f8fe2/3:2/w_5481,h_3654,c_limit/Summit_Exterior.jpg" },
+    { id: 4, nombre: "Obras de teatro", precio: 150, imagen: "https://static.anuevayork.com/wp-content/uploads/2016/10/13144808/El-Rey-Leon-Broadway-comprar-entradas.jpg" },
 ];
 
 experiencias.forEach(experiencia => {
     let div = document.createElement("div");
+    div.className = 'contenedor-div'
     div.innerHTML = `
     <h2> Id: ${experiencia.id} </h2>
+    <img src= "${experiencia.imagen}" alt=${experiencia.nombre}/>
     <h3 class='experiencia-producto'> Nombre: ${experiencia.nombre} </h3>
     <h3 class='experiencia-producto'> precio: $${experiencia.precio} </h3>
     <button class= 'boton-carrito-agregar' onclick='agregarProducto(${experiencia.id})'> Agregar al carrito </button>
     `
 
     let main = document.querySelector('main')
-
     main.append(div);
 });
 
