6cbbe76ba239fb58d9478de741559a5413d33591
Jimena Krochmal <jimenakrochmal@gmail.com>
2023-09-12 08:59:36 -0300

    primera carga del proyecto final

--- carrito.js
+++ carrito.js
@@ -5,13 +5,43 @@ if (carritoGuardado) {
     carrito = JSON.parse(carritoGuardado);
 }
 
+
 const borrarProducto = producto => {
-    const productoEnCarrito = carrito.filter(prod => prod.id !== producto);
-    console.log(productoEnCarrito);
-    if (productoEnCarrito) {
-        localStorage.setItem("carrito", JSON.stringify(productoEnCarrito));
-        location.reload()
-    }
+    Swal.fire({
+        title: '¿Estas seguro/a?',
+        text: "Una vez borrado tendrá que ser ingresado nuevamente",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: '¡Sí, borralo!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            const productoEnCarrito = carrito.filter(prod => prod.id !== producto);
+            if (productoEnCarrito) {
+                localStorage.setItem("carrito", JSON.stringify(productoEnCarrito));
+                location.reload()
+            }
+            Swal.fire(
+                '¡Borrad0!',
+                'La experiencia ha sido borrado',
+                'success'
+            )
+        }
+    })
+
+}
+
+if (carrito.length === 0) {
+    let div = document.createElement("div");
+    div.className = 'no-productos-div'
+    div.innerHTML = `
+        <h2> No hay productos en el carrito</h2>
+    `
+
+    let section = document.querySelector('section')
+
+    section.append(div);
 }
 
 carrito.forEach(carrito => {
@@ -28,4 +58,10 @@ carrito.forEach(carrito => {
 
     section.append(div);
 });
+
+
+
+
+
+
 
--- main.js
+++ main.js
@@ -1,33 +1,31 @@
 
-//creo y cargo las experiencias en mi HTML
-const experiencias = [
-    { id: 1, nombre: "Monumentos", precio: 100, imagen: "https://cdn.milenio.com/uploads/media/2020/05/21/la-estructura-ha-sido-sede.jpg" },
-    { id: 2, nombre: "Parques", precio: 50, imagen: "https://drive.google.com/uc?export=view&id=1xIxVa7sKmx6_8-cLg1XkYawcnZmS2nFg" },
-    { id: 3, nombre: "Rascacielos", precio: 56, imagen: "https://media.architecturaldigest.com/photos/61708d2c231989f8d88f8fe2/3:2/w_5481,h_3654,c_limit/Summit_Exterior.jpg" },
-    { id: 4, nombre: "Obras de teatro", precio: 150, imagen: "https://static.anuevayork.com/wp-content/uploads/2016/10/13144808/El-Rey-Leon-Broadway-comprar-entradas.jpg" },
-];
+let main = document.querySelector('main')
+let experiencias = [];
 
-experiencias.forEach(experiencia => {
-    let div = document.createElement("div");
-    div.className = 'contenedor-div'
-    div.innerHTML = `
-    <h2> Id: ${experiencia.id} </h2>
-    <img src= "${experiencia.imagen}" alt=${experiencia.nombre}/>
-    <h3 class='experiencia-producto'> Nombre: ${experiencia.nombre} </h3>
-    <h3 class='experiencia-producto'> precio: $${experiencia.precio} </h3>
-    <button class= 'boton-carrito-agregar' onclick='agregarProducto(${experiencia.id})'> Agregar al carrito </button>
-    `
+fetch("./datos.json")
+    .then((response) => response.json())
+    .then((datos) => {
+        experiencias = datos;
+        datos.forEach(experiencia => {
+            let div = document.createElement("div");
+            div.className = 'contenedor-div'
+            div.innerHTML = `
+            <h2> Id: ${experiencia.id} </h2>
+            <img src= "${experiencia.imagen}" alt=${experiencia.nombre}/>
+            <h3 class='experiencia-producto'> Nombre: ${experiencia.nombre} </h3>
+            <h3 class='experiencia-producto'> precio: $${experiencia.precio} </h3>
+            <button class= 'boton-carrito-agregar' onclick='agregarProducto(${experiencia.id})'> Agregar al carrito </button>
+            `
 
-    let main = document.querySelector('main')
-    main.append(div);
-});
-
-
+            return main.append(div)
+        })
+    })
 
 let carrito = [];
 
 // Cargo el carrito en caso de que ya tenga algo ingresado 
-const carritoGuardado = localStorage.getItem("carrito");
+const carritoGuardado = localStorage.getItem("carrito") || ""
+
 if (carritoGuardado) {
     carrito = JSON.parse(carritoGuardado);
 }
@@ -39,6 +37,13 @@ const agregarProducto = producto => {
     if (!productoEnCarrito) {
         carrito.push(productoAgregado);
         localStorage.setItem("carrito", JSON.stringify(carrito));
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Your work has been saved',
+            showConfirmButton: true,
+            timer: 15000
+        })
     }
 }
 
